aa55f1b712989cce1b19fcb70e465e17a7f887d4
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-16 01:30:18 -0700

    :sparkles: [feat]: implement custom hook to detect clickaway

---
+++ src/utils/hooks.js
@@ -0,0 +1,35 @@
+import { useCallback, useEffect, useRef } from "react"
+
+export const useClickAway = (fn) => {
+   const ref = useRef(null);
+
+   const escapeListner = useCallback((e) => {
+      if(e.key === "Escape" || e.key === "esc") {
+         fn();
+      }
+
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, []);
+
+   const clickListner = useCallback((e) => {
+      if(!ref.current.contains(e.target)) {
+         fn();
+      }
+
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [ref.current]);
+
+   useEffect(() => {
+      document.addEventListener("keyup", escapeListner);
+      document.addEventListener("click", clickListner);
+
+      return () => {
+         document.removeEventListener("keyup", escapeListner);
+         document.removeEventListener("click", clickListner);
+      }
+
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, []);
+
+   return ref;
+}
