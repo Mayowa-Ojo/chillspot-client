d0c81ebefc169305ec6ea67e4dfcb53ffef4d706
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-17 00:19:28 -0700

    :sparkles: [feat]: export stories and signup containers

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,5 @@
 export { default as Home } from "./Home";
+export { default as Stories } from "./Stories";
 export { default as Login } from "./Login";
+export { default as Signup } from "./Signup";
 export { default as NotFound } from "./NotFound";
