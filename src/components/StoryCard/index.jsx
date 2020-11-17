3c869ce4ee3912e4037f79f51ded162ecb461968
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-16 20:52:13 -0700

    :sparkles: [feat]: add variation for full-view

--- src/components/StoryCard/index.jsx
+++ src/components/StoryCard/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import tw from "twin.macro";
 
-import { CardWrapper, CardOverlay } from "./styles";
-import { Text, FlexBox, Bucket } from "../../components";
+import { CardWrapper, CardOverlay, CardThumbnail, CardDetails } from "./styles";
+import { Text, FlexBox, Bucket, Avatar, Image } from "../../components";
 import { ReactComponent as StarIcon } from "../../assets/svg/star.svg";
 import { ReactComponent as HeartIcon } from "../../assets/svg/heart.svg";
-import { ReactComponent as BookmarkIcon } from "../../assets/svg/bookmark.svg";
+import { ReactComponent as SaveIcon } from "../../assets/svg/save.svg";
 import { ReactComponent as MarkerIcon } from "../../assets/svg/marker.svg";
+import { ReactComponent as CommentIcon } from "../../assets/svg/comment.svg";
 
-const StoryCard = () => {
+const StoryCard = ({ isSlider }) => {
    return (
       <CardWrapper>
+      <CardThumbnail isSlider={isSlider}>
          <CardOverlay>
             <FlexBox css={[tw`justify-between items-start`]}>
                <Bucket>
@@ -22,18 +24,36 @@ const StoryCard = () => {
                </Bucket>
 
                <Bucket css={[tw`flex flex-col items-center`]}>
-                  <HeartIcon css={[tw`stroke-current text-white w-4 h-4 mt-1`]} />
-                  <Bucket css={[tw`inline-flex items-center justify-center rounded-full bg-chill-indigo1 w-6 h-6 mt-2`]}>
-                     <BookmarkIcon css={[tw`fill-current text-white w-3 h-3`]} />
-                  </Bucket>
+                  <HeartIcon css={[tw`stroke-current text-white w-4 h-4 mt-1 mb-2`]} />
                </Bucket>
             </FlexBox>
 
-            <FlexBox css={[tw`justify-start`]}>
-               <MarkerIcon css={[tw`fill-current text-white w-4 h-4`]} />
-               <Text css={[tw`text-c-12 font-semibold text-white ml-1`]}>Ontario, Canada</Text>
+            <FlexBox css={[tw`justify-between`]}>
+               <Bucket as="span" css={[tw`inline-flex items-center`]}>
+                  <MarkerIcon css={[tw`fill-current text-white w-4 h-4`]} />
+                  <Text css={[tw`text-c-12 font-semibold text-white ml-1`]}>Ontario, Canada</Text>
+               </Bucket>
+               { isSlider && <Bucket css={[tw`inline-flex items-center justify-center`]}>
+                  <SaveIcon css={[tw`fill-current text-white w-5 h-5`]} />
+               </Bucket>}
             </FlexBox>
          </CardOverlay>
+      </CardThumbnail>
+
+      <CardDetails>
+         <Avatar>
+            <Image src="https://images.pexels.com/photos/61100/pexels-photo-61100.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb" />
+         </Avatar>
+         <Text css={[tw`font-semibold ml-2 flex-auto`]}>Anna Schoger</Text>
+         <Bucket as="span" css={[tw`ml-2 text-c-12 font-medium text-chill-gray4 inline-flex items-center`]}>
+            <CommentIcon css={[tw`fill-current text-chill-gray4 mr-2 `]} />
+            14
+         </Bucket>
+         <Bucket as="span" css={[tw`ml-2 text-c-12 font-medium text-chill-gray4 inline-flex items-center`]}>
+            <HeartIcon css={[tw`fill-current text-chill-gray4  mr-2 w-4 h-4`]} />
+            234
+         </Bucket>
+      </CardDetails>
       </CardWrapper>
    )
 }
--- src/components/StoryCard/styles.js
+++ src/components/StoryCard/styles.js
@@ -1,15 +1,25 @@
 import tw, { styled, css } from "twin.macro";
 
-export const CardWrapper = styled.div(() => [
+export const CardWrapper = styled.div``;
+
+export const CardThumbnail = styled.div(({ isSlider }) => [
    tw`relative overflow-hidden mr-8`,
    css`
-      border-radius: 25px;
       background-image: url(https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/london-2393098_1280.jpg);
       background-repeat: no-repeat;
       background-position: center;
       background-size: cover;
+   `,
+
+   isSlider && css`
+      width: 280px;
+      height: 230px;
+      border-radius: 8px;
+   `,
+   !isSlider && css`
       width: 189px;
       height: 258px;
+      border-radius: 25px;
    `
 ]);
 
@@ -18,4 +28,8 @@ export const CardOverlay = styled.div`
 
    background-color: rgba(0, 0, 0, 0.6);
    transition: opacity .2s linear;
+`;
+
+export const CardDetails = styled.div`
+   ${tw`flex items-center mt-4`}
 `;
