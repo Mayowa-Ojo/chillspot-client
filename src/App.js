6f15f9a42ffb23403b2fac84ed2713811b60392f
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-17 23:49:44 -0700

    :sparkles: [feat]: register modal component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { GlobalStyles } from "twin.macro";
 import { BrowserRouter as Router, Route , Switch} from "react-router-dom";
 
-import { Home, Stories, Login, Signup, NotFound } from "./containers";
-import { Navbar } from "./components";
+import { Home, Stories, Login, Signup, Story, NotFound } from "./containers";
+import { Navbar, ModalWrapper } from "./components";
 
 function App() {
+   const modalComponents = {
+      story: Story
+   }
+
+   // NOTE: current-modal value will be fetched from global state
+   const CurrentModal = modalComponents["story"];
+
    return (
       <Router>
          <GlobalStyles />
@@ -17,6 +24,10 @@ function App() {
             <Route path="/stories" exact component={Stories} />
             <Route path="*" component={NotFound}></Route>
          </Switch>
+
+         <ModalWrapper>
+            {<CurrentModal />}
+         </ModalWrapper>
       </Router>
    );
 }
