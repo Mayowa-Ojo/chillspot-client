03e83aed4578dfdf8e70038292466a1470daeff9
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-22 08:23:21 -0700

    :sparkles: [feat]: add UI view for authenticated user

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,26 +1,94 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link, useLocation } from "react-router-dom";
 import tw from "twin.macro";
 
-import { Nav, NavItem } from "./styles";
-import { Bucket } from "../../components";
+import { Nav, NavItem, ProfilePopoverItem, ProfilePopoverWrapper, SearchBox, SearchInput } from "./styles";
+import { Avatar, Bucket, Divider, Image, Popover } from "..";
 import { ReactComponent as Logo } from "../../assets/svg/logo.svg";
+import { ReactComponent as FeatherIcon } from "../../assets/svg/feather.svg";
+import { ReactComponent as SearchIcon } from "../../assets/svg/search.svg";
+import { ReactComponent as HeartIcon } from "../../assets/svg/heart.svg";
+import { ReactComponent as FolderIcon } from "../../assets/svg/folder.svg";
+import { ReactComponent as ExitIcon } from "../../assets/svg/exit.svg";
+import { StoreContext } from "../../store";
+import types from "../../store/types";
 
 const Navbar = () => {
+   const context = useContext(StoreContext);
+   const { state: { auth }, dispatch} = context;
    const { pathname } = useLocation();
 
+   const handleNewStory = () => {
+      dispatch({
+         namespace: "modal",
+         type: types.SET_MODAL_COMPONENT,
+         payload: "newStory"
+      });
+
+      dispatch({
+         namespace: "modal",
+         type: types.TOGGLE_MODAL,
+      });
+   }
+
    return (
-      <Nav hasShadow={pathname != "/"} hidden={pathname == "/login" || pathname == "/signup"}>
+      <Nav hasShadow={pathname !== "/"} hidden={pathname === "/login" || pathname === "/signup"}>
          <Link to="/">
             <Logo />
          </Link>
-         <Bucket as="span" css={[tw`inline-flex items-center`]}>
-            <NavItem isTransparent><Link to="/stories">Stories</Link></NavItem>
-            <NavItem isTransparent><Link to="/login">Login</Link></NavItem>
-            <NavItem><Link to="/signup">Signup</Link></NavItem>
-         </Bucket>
+         { !auth.isLoggedIn ?
+            <Bucket as="span" css={[tw`inline-flex items-center`]}>
+               <NavItem isTransparent><Link to="/stories">Stories</Link></NavItem>
+               <NavItem isTransparent><Link to="/login">Login</Link></NavItem>
+               <NavItem><Link to="/signup">Signup</Link></NavItem>
+            </Bucket>
+            :
+            <Bucket as="span" css={[tw`inline-flex items-center`]}>
+               <SearchBox>
+                  <SearchIcon css={[tw`absolute fill-current text-chill-gray4 mt-2 ml-3 left-0`, "width: .8rem; height: .8rem"]}/>
+                  <SearchInput placeholder="Search..." />
+               </SearchBox>
+               <Avatar css={[tw`w-8 h-8 ml-4 cursor-pointer`]}>
+                  <Popover placement="bottom" content={<ProfilePopover />}>
+                     <Image src="https://uifaces.co/our-content/donated/n4Ngwvi7.jpg" alt="profile image"/>
+                  </Popover>
+               </Avatar>
+               <NavItem css={[tw`py-2 px-3 ml-4 rounded-md`]} onClick={handleNewStory}>
+                  <FeatherIcon css={[tw`fill-current text-white w-4 h-4 mr-2`]}/>
+                  New Story
+               </NavItem>
+            </Bucket>
+         }
       </Nav>
    )
 }
+
+const ProfilePopover = () => (
+   <ProfilePopoverWrapper>
+      <ProfilePopoverItem css={[tw`font-semibold`]}>
+         Jonathan Buckenbeur
+      </ProfilePopoverItem>
+      <ProfilePopoverItem>Edit Profile</ProfilePopoverItem>
+      <Bucket as="span" css={[tw`px-8 block`]}>
+         <Divider css={[tw`my-3`]}/>
+      </Bucket>
+      <ProfilePopoverItem css={[tw`w-full flex items-center`]}>
+         <HeartIcon css={[tw`fill-current text-chill-gray4 mr-2 w-4 h-4`]}/>
+         My Likes
+      </ProfilePopoverItem>
+      <ProfilePopoverItem css={[tw`w-full flex items-center`]}>
+         <FolderIcon css={[tw`fill-current text-chill-gray4 mr-2 w-4 h-4`]}/>
+         My Collections
+      </ProfilePopoverItem>
+      <Bucket as="span" css={[tw`px-8 block`]}>
+         <Divider css={[tw`my-3`]}/>
+      </Bucket>
+      <ProfilePopoverItem>Account Settings</ProfilePopoverItem>
+      <ProfilePopoverItem css={[tw`w-full flex items-center`]}>
+         <ExitIcon css={[tw`fill-current text-chill-gray4 mr-2`]}/>
+         Sign Out
+      </ProfilePopoverItem>
+   </ProfilePopoverWrapper>
+)
 
 export default Navbar;
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,4 +1,4 @@
-import tw, { styled, css } from "twin.macro";
+import tw, { styled, css, theme } from "twin.macro";
 
 export const Nav = styled.nav(({ hasShadow, hidden }) => [
    tw`bg-chill-gray1 h-16 w-full flex items-center justify-between px-8`,
@@ -12,6 +12,43 @@ export const Nav = styled.nav(({ hasShadow, hidden }) => [
 ])
 
 export const NavItem = styled.button`
-   ${tw`px-2 py-1 rounded bg-chill-indigo1 text-c-15 text-white font-semibold mr-2 hover:bg-opacity-75`}
+   ${tw`px-2 py-1 inline-flex items-center rounded bg-chill-indigo1 text-c-15 text-white font-semibold mr-2 hover:bg-opacity-75`}
    ${({ isTransparent }) => isTransparent && tw`bg-transparent text-chill-gray4 hover:(text-chill-indigo2)`}
 `
+
+export const SearchBox = styled.div`
+   ${tw`py-1 bg-chill-gray2 border border-chill-gray2 relative`}
+
+   max-width: 180px;
+   width: 180px;
+   height: 38px;
+   padding: .25rem .5rem .25rem 2rem;
+   border-radius: 8px;
+   &:hover {
+      border: 1px solid ${theme`colors.chill.gray3`};
+      background-color: white;
+      box-shadow: 0px 0px 0px 4px rgba(146, 145, 232, 0.10);
+   }
+   &:focus-within {
+      border: 1px solid ${theme`colors.chill.gray3`};
+      background-color: white;
+      box-shadow: 0px 0px 0px 4px rgba(146, 145, 232, 0.15);
+   }
+   transition: box-shadow .2s linear;
+`;
+
+export const SearchInput = styled.input`
+   ${tw`w-full h-full appearance-none bg-transparent focus:outline-none text-c-12 font-medium text-chill-gray4`}
+
+   &::placeholder {
+      opacity: 75%;
+   }
+`;
+
+export const ProfilePopoverWrapper = styled.ul`
+   ${tw`py-8 w-full`}
+`;
+
+export const ProfilePopoverItem = styled.li`
+   ${tw`px-8 py-2 text-chill-gray4 font-medium text-c-15 hover:bg-chill-gray2`}
+`;
