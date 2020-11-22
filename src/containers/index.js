d427f7cb0dbbf6120d33ea7e408946e596bf1f17
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-22 08:25:43 -0700

    :sparkles: [feat]: export new-story container

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,7 @@
 export { default as Home } from "./Home";
 export { default as Stories } from "./Stories";
+export { default as Story } from "./Story";
+export { default as NewStory } from "./NewStory";
 export { default as Login } from "./Login";
 export { default as Signup } from "./Signup";
 export { default as NotFound } from "./NotFound";
