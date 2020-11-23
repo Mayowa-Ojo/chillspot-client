c0a90c9e432972cec03fb48b2a76bb2e9c4586ca
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-23 00:25:20 -0700

    :sparkles: [feat]: create new-story container

---
+++ src/containers/NewStory/index.jsx
@@ -0,0 +1,162 @@
+import React from 'react';
+import tw from "twin.macro";
+import Select from "react-select";
+import { useDropzone } from "react-dropzone";
+
+import { Bucket, FlexBox, Text , Editor, Tooltip} from "../../components";
+import { FormContainer,
+   FormField,
+   FormLabel,
+   FormInputBox,
+   ImageUpload,
+   ImageUploadMain,
+   ImageUploadThumbnail,
+   MediaTypes,
+   UploadContainer,
+   UploadContainerInner,
+   UploadDetails,
+   FormInput,
+   FormSelectBox,
+   FileInput,
+   CheckBoxLabel,
+   CheckBox,
+   CheckBoxSwitch,
+   UploadFooter,
+   FooterAction,
+} from './styles';
+import { ReactComponent as ImageIcon } from "../../assets/svg/image.svg";
+import { ReactComponent as GifIcon } from "../../assets/svg/gif.svg";
+import { ReactComponent as UploadIcon } from "../../assets/svg/upload.svg";
+import { ReactComponent as QuestionMarkIcon } from "../../assets/svg/question-mark.svg";
+
+const NewStory = () => {
+   const tagOptions = [
+      { value: 'beach', label: 'Beach' },
+      { value: 'resort', label: 'Resort' },
+      { value: 'park', label: 'Park' },
+      { value: 'museum', label: 'Museum' },
+      { value: 'campground', label: 'Campground' },
+      { value: 'mountain', label: 'Mountain' },
+      { value: 'desert', label: 'Desert' },
+   ]
+
+   const handleUpload = (files) => {
+      if(files.length > 1) {
+         console.warn("[WARNING]: you can only upload one file at once.");
+         // display toast notification
+         return
+      }
+      console.log(files)
+      // make api call to upload image to s3 bucket
+   }
+
+   const { getInputProps, getRootProps } = useDropzone({
+      accept: "image/*",
+      onDrop: handleUpload
+   });
+
+   return (
+      <UploadContainer>
+         <UploadContainerInner>
+            <ImageUpload>
+               <ImageUploadMain {...getRootProps()}>
+                  <MediaTypes>
+                     <FlexBox>
+                        <ImageIcon css={[tw`fill-current text-chill-gray3 w-10 h-10`]}/>
+                        <FlexBox isCol css={[tw`items-start ml-3`]}>
+                           <Text css={[tw`font-semibold`]}>One high resolution image</Text>
+                           <Text>PNG, JPG, GIF + Cropping</Text>
+                        </FlexBox>
+                     </FlexBox>
+                     <Bucket as="span" css={[tw`inline-block mx-8 h-8 bg-chill-gray3`, "width: 1px;"]}/>
+                     <FlexBox>
+                        <GifIcon css={[tw`fill-current text-chill-gray3 w-10 h-10`]}/>
+                        <FlexBox isCol css={[tw`items-start ml-3`]}>
+                           <Text css={[tw`font-semibold`]}>Animated GIF</Text>
+                           <Text>400x300, 800x600 or 1600x1200</Text>
+                        </FlexBox>
+                     </FlexBox>
+                  </MediaTypes>
+
+                  <FlexBox isCol>
+                     <UploadIcon className="is-hover" css={[tw`fill-current text-chill-gray3`]} />
+                     <Text as="h1" css={[tw`text-c-24 font-semibold mt-4`]}>Drag and drop an Image</Text>
+                     <Text as="h1" css={[tw`text-c-18 mt-1`]}>Or browse to choose a file</Text>
+                     <Text as="h1" css={[tw`text-c-18 mt-1`]}>(1600x12000 or larger recommended, up to 5MB each)</Text>
+                  </FlexBox>
+                  <FileInput {...getInputProps()}/>
+               </ImageUploadMain>
+               <Text css={[tw`text-c-18 font-semibold mt-6`]}>Multiple Images</Text>
+               <FlexBox css={[tw`justify-between mt-4`]}>
+                  { Array(8).fill().map((_, index) => (
+                        <Bucket key={index}>
+                           <ImageUploadThumbnail {...getRootProps()}>
+                              <FileInput {...getInputProps()}/>
+                           </ImageUploadThumbnail>
+                        </Bucket>
+                     ))
+                  }
+               </FlexBox>
+            </ImageUpload>
+
+            <UploadDetails>
+               <FormContainer>
+                  <FormField>
+                     <FormLabel htmlFor="title">Title</FormLabel>
+                     <FormInputBox>
+                        <FormInput id="title" placeholder="e.g Niagara Falls"/>
+                     </FormInputBox>
+                  </FormField>
+                  <FormField>
+                     <FormLabel htmlFor="location">Location</FormLabel>
+                     <FormInputBox>
+                        <FormInput id="location" placeholder="e.g Ontario, Canada"/>
+                     </FormInputBox>
+                  </FormField>
+                  <FormField>
+                     <Bucket as="span" css={[tw`inline-flex items-center`]}>
+                        <FormLabel htmlFor="tags">Tags</FormLabel>
+                        <Tooltip content="select one or more tags" placement="top">
+                           <QuestionMarkIcon css={[tw`fill-current text-chill-gray4 ml-2 text-opacity-75`]}/>
+                        </Tooltip>
+                     </Bucket>
+                     <FormSelectBox>
+                        <Select 
+                           options={tagOptions}
+                           isMulti
+                           name="tags"
+                           className="select-tags"
+                           classNamePrefix="select"
+                           placeholder="Select tags..."
+                        />
+                     </FormSelectBox>
+                  </FormField>
+                  <FormField>
+                     <FormLabel>Story</FormLabel>
+                     <Editor />
+                  </FormField>
+                  <FormField>
+                     <FormLabel css={[tw`text-c-15`]}>Settings</FormLabel>
+                     <CheckBoxLabel>
+                        <CheckBox />
+                        <CheckBoxSwitch>Only people I follow can comment</CheckBoxSwitch>
+                     </CheckBoxLabel>
+                     <CheckBoxLabel>
+                        <CheckBox />
+                        <CheckBoxSwitch>Comments are disabled</CheckBoxSwitch>
+                     </CheckBoxLabel>
+                  </FormField>
+               </FormContainer>
+            </UploadDetails>
+         </UploadContainerInner>
+
+         <UploadFooter>
+            <FooterAction css={[tw`border border-chill-gray3 hover:text-chill-indigo1`]}>Cancel</FooterAction>
+            <FooterAction css={[tw`bg-chill-indigo2 text-white hover:bg-chill-indigo1`]}>Publish Story</FooterAction>
+         </UploadFooter>
+      </UploadContainer>
+   )
+}
+
+
+export default NewStory;
---
+++ src/containers/NewStory/styles.js
@@ -0,0 +1,245 @@
+import tw, { styled, theme } from "twin.macro";
+
+export const UploadContainer = styled.div`
+   ${tw`bg-white px-4 relative mt-10`}
+
+   border-radius: 12px 12px 0 0;
+   height: calc(100vh - 2.5rem);
+   overflow-y: scroll;
+`;
+
+export const UploadContainerInner = styled.div`
+   ${tw`mx-auto w-full pt-8 pb-32`}
+
+   max-width: 900px;
+`;
+
+export const ImageUpload = styled.div`
+   ${tw`mx-auto w-full`}
+
+   max-width: 800px;
+`;
+
+export const ImageUploadMain = styled.div`
+   ${tw`relative p-6 flex flex-col items-center justify-center cursor-pointer focus:outline-none`}
+
+   height: 600px;
+   border: dashed 2px ${theme`colors.chill.gray3`};
+   border-radius: 8px;
+   transition: border-color .2s linear;
+   .is-hover {
+      transition: color .2s linear;
+   }
+   &:hover {
+      border-color: ${theme`colors.chill.indigo1`};
+      .is-hover {
+         color: ${theme`colors.chill.indigo1`};
+      }
+   }
+`;
+
+export const ImageUploadThumbnail = styled.div`
+   ${tw`cursor-pointer focus:outline-none`}
+
+   width: 90px;
+   height: 68px;
+   border: dashed 2px ${theme`colors.chill.gray3`};
+   border-radius: 8px;
+   transition: border-color .2s linear;
+   &:hover {
+      border-color: ${theme`colors.chill.indigo1`};
+      .is-hover {
+         color: ${theme`colors.chill.indigo1`};
+      }
+   }
+`;
+
+export const MediaTypes = styled.ul`
+   ${tw`flex items-center absolute`}
+
+   top: 24px;
+`;
+
+export const UploadDetails = styled.div`
+   ${tw`mt-8 mx-auto`}
+
+   max-width: 475px;
+`;
+
+export const FormContainer = styled.form`
+   ${tw`w-full`}
+`;
+
+export const FormField = styled.fieldset`
+   ${tw`flex flex-col mt-6`}
+`;
+
+export const FormLabel = styled.label`
+   ${tw`text-c-18 font-semibold text-chill-gray4`}
+`;
+
+export const FormInputBox = styled.div`
+   ${tw`w-full h-10 bg-chill-gray2 mt-2 px-4 py-1`}
+
+   border-radius: 8px;
+   transition: box-shadow .2s linear;
+   transition: background-color .2s linear;
+   &:hover {
+      background-color: white;
+      border: 1px solid ${theme`colors.chill.gray3`};
+      box-shadow: 0px 0px 0px 4px rgba(125, 124, 228, 0.1);
+   }
+   &:focus-within {
+      background-color: white;
+      border: 1px solid ${theme`colors.chill.gray3`};
+      box-shadow: 0px 0px 0px 4px rgba(125, 124, 228, 0.2);
+   }
+`;
+
+export const FormInput = styled.input`
+   ${tw`w-full h-full appearance-none bg-transparent text-c-15 text-chill-gray4 font-medium focus:outline-none`}
+`;
+
+export const FileInput = styled.input``;
+
+export const CheckBox = styled.input.attrs(() => ({
+   type: "checkbox"
+}))``;
+
+export const CheckBoxLabel = styled.label`
+   display: block;
+   margin-top: 12px;
+   cursor: pointer;
+   width: max-content;
+   input {
+      display: none;
+      &:checked + span::after {
+         background: ${theme`colors.indigo.300`};
+         transform: translateX(12px) scale(0.9);
+         transition: background 0.3s ease,
+            transform 0.6s cubic-bezier(0.175, 0.88, 0.32, 1.2);
+      }
+   }
+
+   input:checked + span::before {
+      background: white;
+      border: 2px solid ${theme`colors.indigo.300`};
+   }
+
+   span {
+      padding-left: 40px;
+      height: 22px;
+      position: relative;
+      &::before {
+         content: "";
+         display: block;
+         width: 30px;
+         height: 16.5px;
+         background: ${theme`colors.chill.gray3`};
+         border: 2px solid ${theme`colors.chill.gray3`};
+         border-radius: 11px;
+         position: absolute;
+         top: 0;
+         left: 0;
+         transition: background 0.3s ease, border-color 0.3s ease;
+      }
+      &::after {
+         content: "";
+         display: block;
+         position: absolute;
+         width: 13px;
+         height: 13px;
+         background: white;
+         border-radius: 50%;
+         left: 2px;
+         top: 2px;
+         transition: transform 0.3s ease, opacity 0.2s ease, background 0.2s ease;
+      }
+   }
+`;
+
+export const CheckBoxSwitch = styled.span`
+   ${tw`text-c-12 font-semibold text-chill-gray4`}
+`;
+
+export const FormSelectBox = styled.div`
+   ${tw`w-full mt-2`}
+
+   .select-tags {
+      background: ${theme`colors.chill.gray2`};
+      border: none;
+      border-radius: 8px;
+      height: 40px;
+      &:hover {
+         background-color: white;
+         border: 1px solid ${theme`colors.chill.gray3`};
+         box-shadow: 0px 0px 0px 4px rgba(125, 124, 228, 0.1);
+      }
+      &:focus {
+         background-color: white;
+         border: 1px solid ${theme`colors.chill.gray3`};
+         box-shadow: 0px 0px 0px 4px rgba(125, 124, 228, 0.2);
+      }
+   }
+   .select {
+      &__control {
+         background: transparent;
+         border: none;
+         border-radius: 8px;
+         font-size: ${theme`fontSize.c-15`};
+         font-weight: 500;
+         font-family: 'Montserrat', sans-serif;
+         color: ${theme`colors.chill.gray4`};
+         box-shadow: none !important;
+      }
+      &__multi-value {
+         background: ${theme`colors.gray.400`}
+      }
+      &__multi-value__label {
+         color: ${theme`colors.chill.gray4`};
+      }
+      &__multi-value__remove {
+         cursor: pointer;
+      }
+      &__multi-value__remove:hover {
+         background: ${theme`colors.gray.500`};
+         svg {
+            color: ${theme`colors.chill.gray4`};
+         }
+      }
+      &__placeholder {
+         color: ${theme`colors.chill.gray4`};
+         opacity: 50%;
+      }
+      &__menu {
+         .select__option {
+            color: ${theme`colors.chill.gray4`};
+            font-weight: 500;
+            &:hover {
+               background: ${theme`colors.chill.gray3`};
+            }
+         }
+      }
+      &__indicators {
+         .select__indicator {
+            color: ${theme`colors.gray.500`};
+         }
+         .select__clear-indicator {
+            cursor: pointer;
+         }
+      }
+   }
+`;
+
+export const UploadFooter = styled.footer`
+   ${tw`w-full flex items-center justify-between px-6 fixed bottom-0 left-0`}
+   ${tw`bg-chill-gray1 border-t border-chill-gray3 z-10`}
+
+   height: 70px;
+`;
+
+export const FooterAction = styled.button`
+   ${tw`text-c-15 font-semibold text-chill-gray4`}
+   padding: 10px 16px;
+   border-radius: 8px;
+`;
