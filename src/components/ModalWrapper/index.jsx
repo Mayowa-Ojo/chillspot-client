acabea52915edc7520c26dea34fa21dbdfe45bf1
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-26 12:33:09 -0700

    :recycle: [refactor]: switch modal toggle to use router

--- src/components/ModalWrapper/index.jsx
+++ src/components/ModalWrapper/index.jsx
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import tw from "twin.macro";
 import { Portal } from "react-portal";
 
@@ -6,45 +7,38 @@ import { FlexBox } from '..';
 import { ModalOvarlay } from './styles';
 import { Story, NewStory } from "../../containers";
 import { ReactComponent as XIcon} from "../../assets/svg/x.svg";
-import { StoreContext } from "../../store";
-import types from "../../store/types";
 
-const ModalWrapper = () => {
-   const context = useContext(StoreContext);
-   const { state: { modal }, dispatch } = context;
+const ModalWrapper = ({ component }) => {
+   const history = useHistory();
 
    const modalComponents = {
       story: Story,
       newStory: NewStory
    }
-   const CurrentModal = modalComponents[modal.component];
+   const CurrentModal = modalComponents[component];
 
-   const closeModal = () => {
-      dispatch({
-         namespace: "modal",
-         type: types.TOGGLE_MODAL,
-      });
+   const closeModal = (e) => {
+      e.stopPropagation();
+      history.goBack();
    }
 
    useEffect(() => {
-      if(modal.isOpen) {
-         document.body.classList.add("overflow-hidden");
-         return;
+      document.body.classList.add("overflow-hidden");
+
+      return () => {
+         document.body.classList.remove("overflow-hidden");
       }
 
-      document.body.classList.remove("overflow-hidden")
-   }, [modal.isOpen]);
+   }, []);
 
    return (
       <Portal node={document && document.getElementById('portal-modal')}>
-         { modal.isOpen && 
-            <ModalOvarlay>
-               <FlexBox css={[tw`absolute top-0 right-0 w-10 h-10 z-10 cursor-pointer`]} onClick={closeModal}>
-                  <XIcon css={[tw`fill-current text-white w-3 h-3 opacity-75 hover:opacity-100`]} />
-               </FlexBox>
-               <CurrentModal />
-            </ModalOvarlay>
-         }
+         <ModalOvarlay>
+            <FlexBox css={[tw`absolute top-0 right-0 w-10 h-10 z-10 cursor-pointer`]} onClick={closeModal}>
+               <XIcon css={[tw`fill-current text-white w-3 h-3 opacity-75 hover:opacity-100`]} />
+            </FlexBox>
+            <CurrentModal />
+         </ModalOvarlay>
       </Portal>
    )
 }
