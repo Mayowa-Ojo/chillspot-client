97e4071e3f930989eec9001b7f02d34acd7e652a
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-27 13:20:00 -0700

    :sparkles: [feat]: add type attribute

--- src/components/Styled/Button.js
+++ src/components/Styled/Button.js
@@ -1,6 +1,8 @@
 import tw, { styled } from "twin.macro";
 
-const Button = styled.button`
+const Button = styled.button.attrs(({ type }) => ({
+   type: type || "button"
+}))`
    ${tw`px-2 py-1 rounded bg-chill-indigo1 text-c-15 text-white font-medium hover:bg-chill-indigo1`}
 `;
 
