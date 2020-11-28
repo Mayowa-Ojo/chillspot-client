d863f8c41a69f3a0b7d3209a7bd56883f6f3df7a
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-27 23:39:53 -0700

    :recycle: [refactor]: move Store component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Store from "./store";
 import "./assets/styles.css";
 
 ReactDOM.render(
    // <React.StrictMode>
+   <Store>
       <Router>
          <App />
-      </Router>,
+      </Router>
+   </Store>,
    /* </React.StrictMode>, */
    document.getElementById('root')
 );
