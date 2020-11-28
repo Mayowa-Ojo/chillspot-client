375cd6181fc36e13dae1f4f5e1ac665a444f8842
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-27 23:40:43 -0700

    :sparkles: [feat]: render loading component + implement re-authentication

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { useContext, useEffect } from "react";
 import { GlobalStyles } from "twin.macro";
 import { Route , Switch, useLocation } from "react-router-dom";
 
 import { Home, Stories, Login, Signup, NotFound, Profile } from "./containers";
 import { Navbar, ModalWrapper, Footer, Loading } from "./components";
-import Store from "./store";
+import { StoreContext } from "./store";
+import types from "./store/types";
+import LocalStorage from "./utils/localstorage";
+import httpRequest from "./services/http";
+import { requestEndpoints } from "./constants";
+
+const ls = new LocalStorage();
 
 function App() {
    // handle modal sub-routing
    const location = useLocation();
    const background = location.state && location.state.background;
 
+   const { dispatch } = useContext(StoreContext);
+
+   useEffect(() => {
+      reAuthenticateUser(dispatch);
+   }, [dispatch]);
+
    return (
-      <Store>
+      <>
          <GlobalStyles />
          <Navbar />
 
@@ -29,8 +42,32 @@ function App() {
          }
          <Footer />
          <Loading />
-      </Store>
+      </>
    );
+}
+
+const reAuthenticateUser = async (dispatch) => {
+   const { token, id } = ls.get("user") || {};
+
+   if(!token) {
+      console.warn("[WARNING]: no auth token found");
+      return;
+   }
+
+   try {
+      const { data: response } = await httpRequest(requestEndpoints.users.one(id), {
+         method: "GET",
+      });
+
+      dispatch({
+         namespace: "auth",
+         type: types.SET_USER,
+         payload: { ...response.data.user }
+      });
+
+   } catch (err) {
+      console.error(err.response || err.message);
+   }
 }
 
 export default App;
