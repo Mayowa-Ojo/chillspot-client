7dad1e21036989c01347f843d23485c2d883130a
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-27 23:37:39 -0700

    :sparkles: [feat]: create loading component

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import tw from "twin.macro";
+import { Portal } from "react-portal";
+
+import { StoreContext } from "../../store";
+import { FlexBox } from '..';
+import { ReactComponent as LoadingSpinner } from "../../assets/svg/loading2.svg";
+
+const Loading = () => {
+   const context = useContext(StoreContext);
+   const { state: { global } } = context;
+
+   return (
+      global.status === "loading" && <Portal node={document && document.getElementById('portal-loading')}>
+         <FlexBox css={[tw`w-full h-full absolute inset-0 bg-black bg-opacity-50 z-10`]}>
+            <LoadingSpinner />
+         </FlexBox>
+      </Portal>
+   )
+}
+
+export default Loading;
