888a1c0998ad52e077967fd9feb677c2158b2d58
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-29 15:12:44 -0700

    :lipstick: [style]: ui changes

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import tw from "twin.macro";
 import Slick from "react-slick";
+import { Link, useLocation } from 'react-router-dom';
 
-import { Bucket, Button, Image, Text, FlexBox, StoryCard } from "../../components"
+import { Bucket, Button, Image, Text, FlexBox, StoryCard, Tooltip } from "../../components"
 import { Jumbotron, SearchBox , SearchInput, Marker, StorieSlider, SliderArrow, SliderFilter } from './styles';
 import { ReactComponent as SearchIcon } from "../../assets/svg/search.svg";
 import { ReactComponent as ChevronIcon } from "../../assets/svg/chevron.svg";
@@ -35,7 +36,17 @@ const Home = () => {
       prevArrow: <SliderArrowPrev />
    }
 
+   const locationMarkers = [
+      { location: "39°55’N 32°55’E", top: "10%", left: "100%"},
+      { location: "39°55’N 32°55’E", top: "20%", left: "50%"},
+      { location: "39°55’N 32°55’E", top: "75%", left: "15%"},
+      { location: "39°55’N 32°55’E", top: "68%", left: "92%"},
+      { location: "39°55’N 32°55’E", top: "82%", left: "55%"},
+      { location: "39°55’N 32°55’E", top: "50%", left: "50%"}
+   ]
+
    const [sliderFilter, setSliderFilter] = useState("popular");
+   const location = useLocation();
 
    return (
       <Bucket css={[tw`bg-chill-gray1 h-full`]}>
@@ -45,28 +56,19 @@ const Home = () => {
                <FlexBox isCol css={[tw`w-full h-full absolute inset-0 z-10 w-9/12 mx-8 items-start`]}>
                   <Text css={[tw`text-white font-bold text-4xl leading-tight`]}>The most beautiful places in the world!</Text>
                   <Text css={[tw`text-white font-medium text-c-18`]}>Find your next adventure.</Text>
-                  <Button css={[tw`bg-chill-indigo2 px-4 mt-4 text-c-18 font-semibold inline-flex items-center`]}>
-                     Explore
-                     <ChevronIcon css={[tw`stroke-current text-white ml-2 w-3 h-3`]}/>
-                  </Button>
-                  <Marker top="10%" left="100%">
-                     <MarkerIcon css={[tw`w-5 h-5 fill-current text-white hover:text-yellow-400 cursor-pointer`]}/>
-                  </Marker>
-                  <Marker top="20%" left="50%">
-                     <MarkerIcon css={[tw`w-5 h-5 fill-current text-white hover:text-yellow-400 cursor-pointer`]}/>
-                  </Marker>
-                  <Marker top="75%" left="15%">
-                     <MarkerIcon css={[tw`w-5 h-5 fill-current text-white hover:text-yellow-400 cursor-pointer`]}/>
-                  </Marker>
-                  <Marker top="68%" left="92%">
-                     <MarkerIcon css={[tw`w-5 h-5 fill-current text-white hover:text-yellow-400 cursor-pointer`]}/>
-                  </Marker>
-                  <Marker top="82%" left="55%">
-                     <MarkerIcon css={[tw`w-5 h-5 fill-current text-white hover:text-yellow-400 cursor-pointer`]}/>
-                  </Marker>
-                  <Marker top="50%" left="50%">
-                     <MarkerIcon css={[tw`w-5 h-5 fill-current text-white hover:text-yellow-400 cursor-pointer`]}/>
-                  </Marker>
+                  <Link to="/stories">
+                     <Button css={[tw`bg-chill-indigo2 px-4 mt-4 text-c-18 font-semibold inline-flex items-center`]}>
+                        Explore
+                        <ChevronIcon css={[tw`stroke-current text-white ml-2 w-3 h-3`]}/>
+                     </Button>
+                  </Link>
+                  { locationMarkers.map((marker, idx) => (
+                     <Marker top={marker.top} left={marker.left} key={idx}>
+                        <Tooltip content={marker.location} placement="bottom" isLight>
+                           <MarkerIcon css={[tw`w-5 h-5 fill-current text-white hover:text-yellow-400 cursor-pointer`]}/>
+                        </Tooltip>
+                     </Marker>
+                  ))}
                </FlexBox>
             </Jumbotron>
 
@@ -109,9 +111,11 @@ const Home = () => {
                   </Slick>
                </StorieSlider>
 
+               <Link to={{ pathname: "/x/new", state: { background: location, component: "newStory" }}}>
                <Bucket css={[tw`mt-8`]}>
                   <Button css={[tw`text-c-18 font-semibold px-4 rounded-md bg-chill-indigo2`]}>Share your story</Button>
                </Bucket>
+               </Link>
 
                <FlexBox css={[tw`justify-start mt-8`]}>
                   <GithubIcon css={[tw`w-5 h-5 fill-current text-chill-gray4 mr-4 cursor-pointer hover:text-chill-indigo1`]}/>
@@ -123,8 +127,8 @@ const Home = () => {
                   <Bucket as="span" css={[tw`inline-block w-8 h-8 mr-2 rounded-full overflow-hidden`]}>
                      <Image src="https://avatars1.githubusercontent.com/u/46647640?s=400&u=2be345c5e5575fc1088c232b138660e6749a44f3&v=4"/>
                   </Bucket>
-                  <Text css={[tw`font-semibold`]}>
-                     Designed and developed by <Bucket as="span" css={[tw`cursor-pointer hover:text-chill-indigo1`]}>@unorthodev</Bucket>
+                  <Text css={[tw`font-medium`]}>
+                     Designed and developed by <Text as="a" href="https://github.com/Mayowa-Ojo" css={[tw`cursor-pointer font-semibold hover:text-chill-indigo1`]}>Mayowa Ojo</Text>
                   </Text>
                </FlexBox>
             </Bucket>
