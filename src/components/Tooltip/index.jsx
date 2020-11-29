30b0bbebbc1527b8f2f7ff5dd4bc5a43dd21662a
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-29 14:53:50 -0700

    :lipstick: [style]: ui changes

--- src/components/Tooltip/index.jsx
+++ src/components/Tooltip/index.jsx
@@ -11,6 +11,7 @@ const Tooltip = ({ content, placement, children, isLight }) => {
          placement={placement}
          animation="scale-subtle"
          isLight={isLight}
+         maxWidth={220}
       >
          {children}
       </TooltipWrapper>
