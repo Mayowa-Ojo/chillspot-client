4b4dec4138ec37b92f796480cf1c151e4ea127d6
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-29 14:54:23 -0700

    :sparkles: [feat]: add trigger prop

--- src/components/Popover/index.jsx
+++ src/components/Popover/index.jsx
@@ -3,13 +3,14 @@ import "tippy.js/dist/tippy.css";
 
 import { PopoverWrapper } from "./styles";
 
-const Popover = ({ placement, content, children }) => {
+const Popover = ({ placement, content, trigger, children }) => {
    return (
       <PopoverWrapper
          placement={placement}
          animation="scale-subtle"
          interactive={true}
          content={content}
+         trigger={trigger}
       >
          {children}
       </PopoverWrapper>
