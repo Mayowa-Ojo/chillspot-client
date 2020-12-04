ec1a2d62f39a0d9fd806d8d9dd7803cb3a667981
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-04 09:36:01 -0700

    :lipstick: [style]: ui changes

--- src/components/Styled/Avatar.js
+++ src/components/Styled/Avatar.js
@@ -2,6 +2,8 @@ import tw, { styled } from "twin.macro";
 
 const Avatar = styled.span`
    ${tw`inline-block w-6 h-6 rounded-full overflow-hidden`}
+
+   min-width: 1.5rem;
 `;
 
 export default Avatar;
--- src/components/Styled/Text.js
+++ src/components/Styled/Text.js
@@ -1,7 +1,13 @@
-import tw, { styled } from "twin.macro";
+import tw, { styled, css } from "twin.macro";
 
-const Text = styled.p(() => [
+const Text = styled.p(({ truncate }) => [
    tw`text-c-15 font-medium text-chill-gray4`,
+
+   truncate && css`
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+   `
 ]);
 
 export default Text;
