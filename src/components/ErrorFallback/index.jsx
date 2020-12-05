19be0827f8b728e85cb04ed22dd6c9b5c7859a27
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-05 14:57:00 -0700

    :sparkles: [feat]: create fallback component for error boundary

---
+++ src/components/ErrorFallback/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import tw from "twin.macro";
+
+import { Bucket, FlexBox, Text, Button } from "..";
+import { ReactComponent as Illustration } from "../../assets/svg/404.svg";
+
+const ErrorFallback = ({resetErrorBoundary}) => {
+   return (
+      <FlexBox css={[tw`w-1/2 mx-auto`, "height: calc(100vh - 4rem)"]}>
+         <Illustration />
+         <Bucket css={[tw`ml-8`]}>
+            <Text css={[tw`text-c-24 font-bold`]}>Ooops!</Text>
+            <Text>Something went wrong internally.</Text>
+            <Text as="span" css={[tw`inline-flex items-center mt-6 text-chill-indigo2`]}>
+               <Button 
+                  onClick={resetErrorBoundary}
+                  css={[tw`px-4 py-2 bg-chill-indigo2 font-semibold`]}
+               >Refresh
+               </Button>
+            </Text>
+         </Bucket>
+      </FlexBox>
+   )
+}
+
+export default ErrorFallback;
