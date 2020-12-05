cc57b760d0be032b29de327bedf76473514cdf2d
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-05 14:57:35 -0700

    :sparkles: [feat]: setup error boundary

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Store from "./store";
+import { ErrorFallback } from "./components";
 import "./assets/styles.css";
 
 ReactDOM.render(
    // <React.StrictMode>
    <Store>
       <Router>
+      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => window.location.reload()}>
          <App />
+      </ErrorBoundary>
       </Router>
    </Store>,
    /* </React.StrictMode>, */
