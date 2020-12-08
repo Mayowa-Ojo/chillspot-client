b20b8761b7fce16ebf7abe4a3b823ce4b0e990b9
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-08 00:55:37 -0700

    :recycle: [refactor]: remove action button

--- src/components/NoContentPlaceholder/index.jsx
+++ src/components/NoContentPlaceholder/index.jsx
@@ -4,12 +4,11 @@ import tw, { styled } from "twin.macro";
 import { Text, Button } from "..";
 import { ReactComponent as NoContentIllustration } from "../../assets/svg/no-content.svg";
 
-const NoContentPlaceholder = ({ message, action }) => {
+const NoContentPlaceholder = ({ message }) => {
    return (
       <NoStoriesPlaceholder>
          <NoContentIllustration />
          <Text css={[tw`text-c-18 font-semibold mt-10`]}>{message}</Text>
-         <Button css={[tw`mt-4 py-2 px-4 bg-chill-indigo2 rounded-lg`]}>{action.text}</Button>
       </NoStoriesPlaceholder>
    );
 }
