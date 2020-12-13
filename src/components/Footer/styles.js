35f8d75f62b3b3abcc9f5e57a03f83f3a76a5d99
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-13 02:33:40 -0700

    :lipstick: [style]: ui changes

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,7 +1,7 @@
 import tw, { styled } from "twin.macro";
 
 export const FooterContainer = styled.footer`
-   ${tw`flex justify-center p-8 bg-chill-gray1`}
+   ${tw`flex justify-center p-8 bg-chill-gray1 mt-16`}
 `;
 
 export const FooterContent = styled.div`
